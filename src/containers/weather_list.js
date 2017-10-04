057cd35d13adaf5a38de726953a2d2b8dbf3966e
Pooja Shroff <poshroff@paypalcorp.com>
2017-10-04 14:26:34 -0700

    Final addition - Google Maps

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
-
+import Chart from "../components/chart";
+import GoogleMap from "../components/google_map";
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const {lon, lat} = cityData.city.coord;
 
-    renderWeather(cityData) {
-        const name = cityData.city.name;
-        return (
-            <tr key={name}>
-                <td>{name}</td>
-            </tr>
-        );
-    }
+    return (
+      <tr key={name}>
+        <td><GoogleMap lon={lon} lat={lat}></GoogleMap></td>
+        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
+      </tr>
+    );
+  }
 
   render() {
     return (
@@ -20,23 +28,19 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
-        <tbody>
-            {this.props.weather.map(this.renderWeather)}
-
-        </tbody>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
 }
 
-
-function mapStateToProps({ weather }){
-    return { weather }; //{weather: weather}
+function mapStateToProps({ weather }) {
+  return { weather }; //{weather: weather}
 }
 
 WeatherList.propTypes = {
